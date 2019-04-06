3583ab3ae7ca919c3c7f28b84efa4edf62ead7df
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-06 20:41:14 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,21 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand 
+    BRANDS.each{ |shoe_brand|
+    BRANDS.include?(shoe_brand)
+    }
+    BRANDS.uniq!
+  end
+  
+  def brand=(brand_name)
+  
+    @brand= brand_name
   end
 
   def cobble
